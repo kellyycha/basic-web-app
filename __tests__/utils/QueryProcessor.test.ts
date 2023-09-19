07599338c440d3de678e474cf9b0bafedfcde03f
kellyycha <65841983+kellyycha@users.noreply.github.com>
2023-09-19 16:18:11 -0400

    added add and multiply functionality

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -24,4 +24,14 @@ describe("QueryProcessor", () => {
             "kcha"
           ));
     });
+
+    test('should add numbers', () => {
+        expect(QueryProcessor("What is 5 plus 52?")).toEqual("57");
+        expect(QueryProcessor("What is 12 plus 1?")).toEqual("13");
+    });
+
+    test('should multiply numbers', () => {
+        expect(QueryProcessor("What is 5 multiplied by 2?")).toEqual("10");
+        expect(QueryProcessor("What is 12 multiplied by 1?")).toEqual("12");
+    });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,5 +12,19 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
+
+  const multiplyMatch = query.match(/What is (\d+) multiplied by (\d+)/);
+  if (multiplyMatch) {
+    const x: number = parseInt(multiplyMatch[1]);
+    const y: number = parseInt(multiplyMatch[2]);
+    return (x*y).toString();
+  }
+
   return "";
 }
